b4140ea9a0f83db5d26c92bb9d508cb8ad87c516
matrayu <northridge614@gmail.com>
2019-07-20 10:41:49 -0700

    beginning frontend setup. added services from thingful

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -7,10 +7,43 @@ import RegisterForm from '../RegisterForm/RegisterForm';
 import RegisterName from '../RegisterName/RegisterName';
 import RegisterDob from '../RegisterDob/RegisterDob';
 import NumerologlyReport from '../NumerologyReport/NumerologyReport';
+import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service';
+import IdleService from '../../services/idle-service.js';
 
 import './App.css';
 
 class App extends Component {
+  state = { hasError: false }
+
+  static getDerivedStateFromError(error) {
+    console.error(error)
+    return { hasError: true }
+  }
+
+  componentDidMount() {
+    IdleService.setIdleCallback(this.logoutFromIdle)
+
+    if (TokenService.hasAuthToken()) {
+      IdleService.regiserIdleTimerResets()
+      TokenService.queueCallbackBeforeExpiry(() => {
+        AuthApiService.postRefreshToken()
+      })
+    }
+  }
+
+  componentWillMount() {
+    IdleService.unRegisterIdleResets()
+    TokenService.clearCallbackBeforeExpiry()
+  }
+
+  logoutFromIdle = () => {
+    TokenService.clearAuthToken()
+    TokenService.clearCallbackBeforeExpiry()
+    IdleService.unRegisterIdleResets()
+    this.forceUpdate()
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,6 +51,7 @@ class App extends Component {
           <Header />
         </header>
         <main className='App__main'>
+        {this.state.hasError && <p className='red'>There was an error! Oh no!</p>}
           <Switch>
             <Route exact path={'/'} component={HomeSplash} />
             <Route path={'/login'} component={LoginPage} />
--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react';
+import { Button, Input } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service'
 import './LoginForm.css';
 
-class LoginForm extends Component {
-    render() {
-        return (
-            <form className="login-form">
-                <div>Log In</div>
-                <fieldset>
-                    <div>
-                        <label htmlFor="email"></label>
-                        <input id="email" type="email" placeholder="Email Address"></input>
-                    </div>
-
-                    <div>
-                        <label htmlFor="password"></label>
-                        <input id="password" type="password" placeholder="Password"></input>
-                    </div>
-
-                    <div>
-                        <button type="submit">Log In</button>
-                    </div>
-                </fieldset>
-            </form>
-        )
+export default class LoginForm extends Component {
+    static defaultProps = {
+      onLoginSuccess: () => {}
     }
-}
-
-export default LoginForm
+  
+    state = { error: null }
+  
+    handleSubmitJwtAuth = ev => {
+      ev.preventDefault()
+      this.setState({ error: null })
+      const { user_name, password } = ev.target
+      AuthApiService.postLogin({
+        user_name: user_name.value,
+        password: password.value,
+      })
+        .then(res => {
+          user_name.value = ''
+          password.value = ''
+          this.props.onLoginSuccess()
+        })
+    }
+  
+    render() {
+      const { error } = this.state
+      return (
+        <form className='LoginForm' onSubmit={this.handleSubmitJwtAuth}>
+          <div role='alert'>
+            {error && <p className='red'>{error}</p>}
+          </div>
+          <div className='user_name'>
+            <label htmlFor='LoginForm__user_name'>User name</label>
+            <Input required name='user_name' id='LoginForm__user_name'></Input>
+          </div>
+          <div className='password'>
+            <label htmlFor='LoginForm__password'>Password</label>
+            <Input required name='password' type='password' id='LoginForm__password'></Input>
+          </div>
+          <Button type='submit'>Login</Button>
+        </form>
+      )
+    }
+  }
