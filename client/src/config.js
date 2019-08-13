9be4904b35b0b49c687145114789fe7537c71c29
matrayu <northridge614@gmail.com>
2019-08-12 20:01:42 -0700

    added production postgrator config and added it to a migration step in package.json

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'http://localhost:8000/api', //change to deployed HEROKU url don't forguet /api
     TOKEN_KEY: 'numerology-client-auth-token',
   }
