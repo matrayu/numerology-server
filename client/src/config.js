8c0cfdc5324a6e556ff03f1d35d4499f660134d5
matrayu <northridge614@gmail.com>
2019-08-12 21:10:20 -0700

    updated api endpoint

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-export default {
-    API_ENDPOINT: 'https://ancient-cove-47706.herokuapp.com/api', //'http://localhost:8000/api', //change to deployed HEROKU url don't forget /api
-    TOKEN_KEY: 'numerology-client-auth-token',
-  }
+module.exports = {
+  API_ENDPOINT: 'https://ancient-cove-47706.herokuapp.com/api', 
+  //API_ENDPOINT: 'http://localhost:8000/api',
+  TOKEN_KEY: 'numerology-client-auth-token',
+}
