c3b6ba53ad575b3b30e17b6871e878099ff5e1da
matrayu <northridge614@gmail.com>
2019-08-12 17:49:52 -0700

    adds static hosting

--- db/src/server.js
+++ db/src/server.js
@@ -9,6 +9,11 @@ const db = knex({
 
 app.set('db', db)
 
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
