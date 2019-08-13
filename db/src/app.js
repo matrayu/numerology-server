d59f7fabb4ac59de1a0e21bdf60183007c28d589
matrayu <northridge614@gmail.com>
2019-08-12 17:53:10 -0700

    modifying static location

--- db/src/app.js
+++ db/src/app.js
@@ -19,7 +19,7 @@ app.use(helmet())
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+    app.use(express.static("../../client/build"));
   }
 
 app.use('/api/users', userRouter)
