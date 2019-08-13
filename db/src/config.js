542d4f8d9d640349db6b2328bdfa3ba7641d8dbe
matrayu <northridge614@gmail.com>
2019-08-12 19:42:25 -0700

    modifying DB_URL for heroku

--- db/src/config.js
+++ db/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://matrayu@localhost/numerology',
+    DB_URL: process.env.DB_DATABASE_URL || 'postgresql://matrayu@localhost/numerology', //change from DB_UL to DATABASE_URL prior to deploying
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
