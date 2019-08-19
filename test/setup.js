ce80c8db3618270292354b5cbc0f49213a4cbb4a
matrayu <northridge614@gmail.com>
2019-08-19 00:04:31 -0700

    Update setup.js

--- test/setup.js
+++ test/setup.js
@@ -5,7 +5,7 @@ process.env.NODE_ENV = 'test'
 process.env.JWT_SECRET = 'change-this-secret'
 process.env.JWT_EXPIRY = '3h'
 process.env.TEST_DB_URL = process.env.TEST_DB_URL
-  || "postgresql://matrayu@localhost/numerology-test"
+  || "postgresql://matrayu@localhost/numerology_test"
 
 const { expect } = require('chai')
 const supertest = require('supertest')
